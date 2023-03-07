9f2b97739781cbe78b41f74cc27d05f1a13bc324
Giedrius Išora <g.isora@outlook.com>
2023-03-07 14:01:26 +0200

    prideti albumreviewlikes ir band image bei cover

--- ptu8_django_api/django_api/serializers.py
+++ ptu8_django_api/django_api/serializers.py
@@ -2,30 +2,57 @@ from rest_framework import serializers
 from . import models
 
 class BandSerializer(serializers.ModelSerializer):
+    album_count = serializers.SerializerMethodField()
+
+    def get_album_count(self, obj):
+        return models.Album.objects.filter(band=obj).count()
+    
     class Meta:
         model = models.Band
-        fields = ('id', 'name',)
+        fields = ('id', 'name', 'album_count', 'image')
 
 
 class AlbumSerializer(serializers.ModelSerializer):
+    band_name = serializers.ReadOnlyField(source='band.name')
+    song_count = serializers.SerializerMethodField()
+    review_count = serializers.SerializerMethodField()
+    
+    def get_song_count(self, obj):
+        return models.Song.objects.filter(album=obj).count()
+    
+    def get_review_count(self, obj):
+        return models.AlbumReview.objects.filter(album=obj).count()
+
     class Meta:
         model = models.Album
-        fields = ('id', 'name', 'band_id')
+        fields = ('id', 'name', 'band_name', 'cover', 'band_id', 'song_count', 'review_count')
 
 
 class SongSerializer(serializers.ModelSerializer):
+    band_name = serializers.ReadOnlyField(source='band.name')
+    album_name = serializers.ReadOnlyField(source='album.name')
+    
     class Meta:
         model = models.Song
-        fields = ('id', 'name', 'duration', 'album_id')
+        fields = ('id', 'name', 'band_name', 'album_name', 'album_id', 'duration')
 
 
 class AlbumReviewSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     user_id = serializers.ReadOnlyField(source='user.id')
+    album_name = serializers.ReadOnlyField(source='album.name')
+    comment_count = serializers.SerializerMethodField()
+    like_count = serializers.SerializerMethodField()
+
+    def get_comment_count(self, obj):
+        return models.AlbumReviewComment.objects.filter(album_review=obj).count()
     
+    def get_like_count(self, obj):
+        return models.AlbumReviewLike.objects.filter(album_review=obj).count()
+
     class Meta:
         model = models.AlbumReview
-        fields = ('id', 'user', 'user_id', 'album_id', 'content', 'score')
+        fields = ('id', 'user', 'user_id', 'album_name', 'album_id', 'content', 'score', 'comment_count', 'like_count')
 
 
 class AlbumReviewCommentSerializer(serializers.ModelSerializer):
@@ -38,9 +65,6 @@ class AlbumReviewCommentSerializer(serializers.ModelSerializer):
 
 
 class AlbumReviewLikeSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
-    user_id = serializers.ReadOnlyField(source='user.id')
-    
     class Meta:
         model = models.AlbumReviewLike
-        fields = ('id', 'user', 'user_id', 'album_review_id')
+        fields = ('id', )
--- ptu8_django_api/ptu8_django_api/urls.py
+++ ptu8_django_api/ptu8_django_api/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('django_api.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
